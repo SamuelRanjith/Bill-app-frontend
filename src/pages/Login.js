36d42870016afb03629cc2e0a850c8f1425d216f
samuelranjith <samuelranjith11@gmail.com>
2022-08-01 17:37:25 +0530

    frontend

---
+++ src/pages/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container, Row, Col } from "react-bootstrap";
+
+function Login() {
+  
+     return(
+          <Container>
+               <Rom>
+               <Col mod ={6}></Col>
+               <Col mod ={6}></Col>
+               </Rom>
+          </Container>
+     )
+}
+
+export default Login
